996674409751af9492e97e95c1fd80396aaa8f86
Ruslan <r.nikitin.00@mail.ru>
2020-11-03 16:40:08 +0300

    Смена разрешения изображения и нахождение медианной точки

--- main.cpp
+++ main.cpp
@@ -15,16 +15,6 @@ public:
     double b;
 };
 
-struct Point
-{
-public:
-    double x;
-    double y;
-};
-
-int W = 480;
-int H = 320;
-
 // Нахожднние коэффициентов m и b уравнения прямой y = mx +
 
 Line lineCalc(double vx, double vy, double x0, double y0)
@@ -40,7 +30,7 @@ Line lineCalc(double vx, double vy, double x0, double y0)
 
 // Нахождение угла между двумя прямыми, заданными уравнениями ax + by + c = 0
 
-double angle(Point pt1, Point pt2)
+double angle(Point2D pt1, Point2D pt2)
 {
     double x1 = pt1.x;
     double y1 = pt1.y;
@@ -57,10 +47,10 @@ double angle(Point pt1, Point pt2)
 
 // Нахождение точки пересечения двух прямых, заданных уравнениями y = mx + b
 
-Point lineIntersect (Line L, Line R)
+Point2D lineIntersect (Line L, Line R)
 {
     // Для нахождения пересечения будем решать систему уравнений методом Крамера
-    Point intersect;
+    Point2D intersect;
     double a1 = -L.m;
     double b1 = 1;
     double c1 = L.b;
@@ -78,19 +68,56 @@ Point lineIntersect (Line L, Line R)
     return intersect;
 }
 
+double* QueueToArray(queue <double> q, int num)
+{
+    double *arr = new double[num];
+    for(int i = 0; i < num; i++)
+    {
+        arr[i] = q.front();
+        q.pop();
+    }
+    return arr;
+}
+
 void process(Image im)
 {
-    int x = W;
-    int y = H;
-    int bw_width = 110;
-    Pixel green;
+    int bw_width = 170; // минимальная длина полосы перехода в px
+
+    Pixel green; // настройки рисования
+    Pixel blue;
+    Pixel yellow;
     green.Set(0,255,0,0);
+    blue.Set(0,250,250,0);
+    yellow.Set(250,250,0,0);
     DrawingStyle standard;
     standard.drawingMode = DrawingMode::HighQuality;
     standard.opacity = 1.0f;
     standard.thickness = 1.0f;
     standard.filled = false;
     standard.pointSize = 1.0f;
+
+    Array<Path> b;
+    queue <double> bxRight;
+    queue <double> bxLeft;
+    queue <double> byRight;
+    queue <double> byLeft;
+    Array<Point2D> bxbyLeftArray;
+    Array<Point2D> bxbyRightArray;
+
+    // Рассчитываем размеры изображения
+
+    Size imSize;
+    ImageToSize(im,imSize);
+    int x = imSize.width;
+    int y = imSize.height;
+
+    // Определяем новое расширение и переводим в него изображение
+
+    double ratio = double(y)/double(x);
+    x = 800;
+    y = int(x * ratio);
+    Image toResize = im;
+    ResizeImage(toResize,x,y,ResizeMethod::NearestNeighbour,im);
 
     // Выделяем белый цвет
 
@@ -101,18 +128,19 @@ void process(Image im)
     // Размываем изображение
     Image erode;
     Region a;
-    int ErodeSize = int(y/40);
+    // int ErodeSize = int(y/60);
     ErodeImage(mask, NIL, NIL, NIL, KernelShape::Box, 1, 1, erode, a);
     SaveImage(erode,NIL,"erode.jpg",0);
 
-    // Находим контуры
+    // Находим контуры различных объектов на изображении
+
     ThresholdToRegion(erode,NIL,128.0f,NIL,0.0f,a);
-    Array<Path> b;
-    queue <double> bxRight;
-    queue <double> bxLeft;
-    queue <double> byRight;
-    queue <double> byLeft;
     RegionContours(a,RegionContourMode::PixelCenters,RegionConnectivity::EightDirections,b);
+
+    // Отсекаем мелкие объекты, рисуем зеленые линии вдоль линий пехеходного перехода
+
+    int num = 0;
+
     for (auto path : b)
     {
         Rectangle2D rect;
@@ -125,15 +153,35 @@ void process(Image im)
             line.point2.x = rect.origin.x + rect.width;
             line.point2.y = rect.origin.y;
             DrawSegment(im,line,NIL,green,standard,MarkerType::Circle,5.0f);
+            bxRight.push(line.point2.x);
+            byRight.push(line.point2.y);
+            bxLeft.push(line.point1.x);
+            byLeft.push(line.point1.y);
+            bxbyLeftArray.PushBack(line.point1);
+            bxbyRightArray.PushBack(line.point2);
+            Circle2D PointCircle;
+            CreateCircle(line.point1,Anchor2D::MiddleCenter,5,PointCircle);
+            DrawCircle(im, PointCircle,NIL,blue,standard);
+            CreateCircle(line.point2,Anchor2D::MiddleCenter,5,PointCircle);
+            DrawCircle(im, PointCircle,NIL,yellow,standard);
+            num++;
         }
     }
+
+    // Находим среднее значение для левого и правого массива
+
+    Point2D median_L;
+    Point2D median_R;
+    PointsMedian(bxbyLeftArray,NIL,10,median_L);
+    PointsMedian(bxbyRightArray,NIL,10,median_R);
+
     SaveImage(im,NIL,"im.jpg",0);
 }
 
 int main()
 {
     Image im;
-    LoadImage("../iii.jpg",0,im);
+    LoadImage("../im2.jpg",0,im);
     process(im);
     return 0;
 }
